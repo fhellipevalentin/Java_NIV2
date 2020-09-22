3f2d186ce5f98f93c7f00960de335c15392752f2
Fhellipe Reis Valentin <fhellipereis@outlook.com>
2020-09-22 12:02:40 -0300

    Informações do caminho do arquivo

---
+++ 6. InformacoesDoCaminhoDoArquivo/FilePath/src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc  = new Scanner(System.in);
+		
+		System.out.print("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		//existem várias maneiras de VISUALIZAR os arquivos e pastas e suas características
+		
+		System.out.println("getPath: " + path.getPath());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getName: " + path.getName());
+		
+		sc.close();
+	}
+
+}
