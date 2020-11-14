202f1920a0be3f3ec7c727c07d2e4c5df7dd6b3d
Fhellipe Reis Valentin <fhellipereis@outlook.com>
2020-11-14 12:45:03 -0300

    

---
+++ 9. ExercicioInterface/ContractsInterface/src/services/ContractService.java
@@ -0,0 +1,21 @@
+package services;
+
+public class ContractService {
+	
+	
+	private OnlinePaymentService onlinePaymentService;
+
+	public ContractService(OnlinePaymentService onlinePaymentService) {
+		this.onlinePaymentService = onlinePaymentService;
+	}
+
+	public OnlinePaymentService getOnlinePaymentService() {
+		return onlinePaymentService;
+	}
+
+	public void setOnlinePaymentService(OnlinePaymentService onlinePaymentService) {
+		this.onlinePaymentService = onlinePaymentService;
+	}
+	
+	
+}
